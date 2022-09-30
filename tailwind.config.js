853ff14d28d1da1eca6345b8b4361a808aa390c4
Patrick M <patrickmatias.silva@hotmail.com>
2022-09-30 14:47:08 -0300

    tailwind-css setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/app.component.html",
+    "./src/app/app.component.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
